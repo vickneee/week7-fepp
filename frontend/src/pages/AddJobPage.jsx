9f8bb5eada5415f4b803f2893bb6a5ad28e5bff3
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-03 10:13:02 +0200

    Refactor AddJobPage form layout and implement job fetching in HomePage

--- frontend/src/pages/AddJobPage.jsx
+++ frontend/src/pages/AddJobPage.jsx
@@ -1,9 +1,7 @@
 const AddJobPage = () => {
-  
   const submitForm = (e) => {
     e.preventDefault();
     console.log("submitForm called");
-   
   };
 
   return (
@@ -11,13 +9,9 @@ const AddJobPage = () => {
       <h2>Add a New Job</h2>
       <form onSubmit={submitForm}>
         <label>Job title:</label>
-        <input
-          type="text"
-          required
-          value=""
-        />
+        <input type="text" required value="" />
         <label>Job type:</label>
-        <select >
+        <select>
           <option value="Full-Time">Full-Time</option>
           <option value="Part-Time">Part-Time</option>
           <option value="Remote">Remote</option>
@@ -25,29 +19,13 @@ const AddJobPage = () => {
         </select>
 
         <label>Job Description:</label>
-        <textarea
-          required
-          value=""
-
-        ></textarea>
+        <textarea required value=""></textarea>
         <label>Company Name:</label>
-        <input
-          type="text"
-          required
-          value=""
-        />
+        <input type="text" required value="" />
         <label>Contact Email:</label>
-        <input
-          type="text"
-          required
-          value=""
-        />
+        <input type="text" required value="" />
         <label>Contact Phone:</label>
-        <input
-          type="text"
-          required
-          value=""
-        />
+        <input type="text" required value="" />
         <button>Add Job</button>
       </form>
     </div>
