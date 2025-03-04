0f008f2babdecc738da6e2a78560d751dc3e1e2f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-04 09:23:31 +0200

    Refactor project structure

---
+++ frontend-no-auth/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/HomePage";
+import AddJobPage from "./pages/AddJobPage";
+import Navbar from "./components/Navbar";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage from "./pages/JobPage";
+import EditJobPage from './pages/EditJobPage';
+
+const App = () => {
+
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/add-job" element={<AddJobPage />} />
+              <Route path="/jobs/:id" element={<JobPage />} />
+              <Route path="/edit-job/:id" element={<EditJobPage />} />
+              <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  
+  export default App;
